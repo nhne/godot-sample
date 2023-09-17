a79e3687850e380ac97240171734cfa7c586a2bb
Kim Doyoung <disoxc21@gmail.com>
2023-09-17 15:02:01 +0900

    Create mob

---
+++ mob.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Minimum speed of the mob in meters per second.
+@export var min_speed = 10
+# Maximum speed of the mob in meters per second.
+@export var max_speed = 18
+
+
+func initialize(start_position, player_posistion):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _physics_process(_delta):
+	move_and_slide()
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
