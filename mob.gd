b038d19083934bcb6ba681fa0b07dde6841fcfb0
Kim Doyoung <disoxc21@gmail.com>
2023-09-17 15:53:31 +0900

    Add squash functionality

--- mob.gd
+++ mob.gd
@@ -6,9 +6,13 @@ extends CharacterBody3D
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
+signal squashed
+
 
 func initialize(start_position, player_position):
-	look_at_from_position(start_position, player_position, Vector3.UP)
+	var player_without_y = player_position
+	player_without_y.y = 0
+	look_at_from_position(start_position, player_without_y, Vector3.UP)
 	rotate_y(randf_range(-PI / 4, PI / 4))
 	
 	var random_speed = randi_range(min_speed, max_speed)
@@ -21,4 +25,9 @@ func _physics_process(_delta):
 
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+
+
+func squash():
+	squashed.emit()
 	queue_free()
--- player.gd
+++ player.gd
@@ -1,8 +1,16 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 8.0
+
+# vertical impulse on jump
+@export var jump_impulse = 10
+
+# vertical impulse applied to cahracter upon bounce
+@export var bounce_impulse = 6
+
+# simulation for graviy
+@export var gravity_simulation_coeff = 3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,14 +19,13 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity * delta * gravity_simulation_coeff
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_impulse
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -29,3 +36,15 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		
+		if collision.get_collider() == null:
+			continue
+		
+		if collision.get_collider().is_in_group('mob'):
+			var mob = collision.get_collider()
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				velocity.y = bounce_impulse
